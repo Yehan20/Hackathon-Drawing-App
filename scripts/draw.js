592951a30e7d853c4394cd3a6205b0adcf2c03fe
Michael Tran <michaelhtran120@gmail.com>
2021-11-18 12:17:46 -0800

    added menu btn functionality for pen tool

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,25 +1,43 @@
 import { resizeCanvas } from "./resize.js";
 
 const toggleBtn = document.getElementById("toggle-btn");
+const penBtn = document.getElementById("pen-btn");
 const toggleBtnImg = document.getElementById("toggle-btn-img");
 const navbar = document.querySelector(".draw-row");
-
-toggleBtn.addEventListener("click", () => {
-    if (navbar.classList.contains("d-none")) {
-        navbar.classList.remove("d-none");
-        toggleBtn.style.left = "50px";
-        toggleBtnImg.src = "images/exit.svg";
-    } else {
-        navbar.classList.add("d-none");
-        toggleBtn.style.left = 0;
-        toggleBtnImg.src = "images/menu.svg";
-    }
-});
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 let inMemCanvas = document.createElement("canvas");
 let inMemCtx = inMemCanvas.getContext("2d");
+
+const state = {
+    isPainting: false,
+    isPenActive: true
+};
+
+// Canvas setup and pen default
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", finishedPosition);
+canvas.addEventListener("mousemove", draw);
+penBtn.style.border = "3px solid red";
+
+// Debounce - This will fire resizeCanvas once after 1 second from the last resize event.
+let timer_id = undefined;
+window.addEventListener("resize", function () {
+    if (timer_id !== undefined) {
+        clearTimeout(timer_id);
+        timer_id = undefined;
+    }
+    timer_id = setTimeout(function () {
+        timer_id = undefined;
+        console.log("resize");
+        resizeCanvas(inMemCanvas, inMemCtx, canvas, ctx);
+    }, 1000);
+});
 
 // Update mouse object {x,y} position everytime it moves relative to canvas size/positioning. (Including resize)
 let mouse = { x: 0, y: 0 };
@@ -36,37 +54,22 @@ function getMousePos(canvas, event) {
         y: event.clientY - rect.top
     };
 }
-// Debounce function that will fire a callback method after 1 second from the last resize event.
-let timer_id = undefined;
-window.addEventListener("resize", function () {
-    if (!timer_id) {
-        clearTimeout(timer_id);
-        timer_id = undefined;
-    }
-    timer_id = setTimeout(function () {
-        timer_id = undefined;
-        resizeCanvas(inMemCanvas, inMemCtx, canvas, ctx);
-    }, 1000);
-});
-
-let painting = false;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 // Draw - Pencil/Pen function
-function startPosition(e) {
-    painting = true;
-    draw(e);
+function startPosition() {
+    state.isPainting = true;
+    // This allows drawing of a dot.
+    draw();
 }
 
 function finishedPosition() {
-    painting = false;
+    state.isPainting = false;
     ctx.save();
     ctx.beginPath();
 }
 
-function draw(e) {
-    if (!painting) return;
+function draw() {
+    if (!state.isPainting) return;
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
     ctx.lineTo(mouse.x, mouse.y);
@@ -74,7 +77,30 @@ function draw(e) {
     ctx.beginPath();
     ctx.moveTo(mouse.x, mouse.y);
 }
+penBtn.addEventListener("click", () => {
+    if (!state.isPenActive) {
+        state.isPenActive = true;
+        canvas.addEventListener("mousedown", startPosition);
+        canvas.addEventListener("mouseup", finishedPosition);
+        canvas.addEventListener("mousemove", draw);
+        penBtn.style.border = "3px solid red";
+    } else {
+        state.isPenActive = false;
+        canvas.removeEventListener("mousedown", startPosition);
+        canvas.removeEventListener("mouseup", finishedPosition);
+        canvas.removeEventListener("mousemove", draw);
+        penBtn.style.border = "none";
+    }
+});
 
-canvas.addEventListener("mousedown", startPosition);
-canvas.addEventListener("mouseup", finishedPosition);
-canvas.addEventListener("mousemove", draw);
+toggleBtn.addEventListener("click", () => {
+    if (navbar.classList.contains("d-none")) {
+        navbar.classList.remove("d-none");
+        toggleBtn.style.left = "50px";
+        toggleBtnImg.src = "images/exit.svg";
+    } else {
+        navbar.classList.add("d-none");
+        toggleBtn.style.left = 0;
+        toggleBtnImg.src = "images/menu.svg";
+    }
+});
