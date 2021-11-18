11cabfd8c20f666865708f23dadcce57e86e60b8
Michael Tran <michaelhtran120@gmail.com>
2021-11-18 11:41:56 -0800

    exported resize function to module

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,3 +1,5 @@
+import { resizeCanvas } from "./resize.js";
+
 const toggleBtn = document.getElementById("toggle-btn");
 const toggleBtnImg = document.getElementById("toggle-btn-img");
 const navbar = document.querySelector(".draw-row");
@@ -19,31 +21,33 @@ const ctx = canvas.getContext("2d");
 let inMemCanvas = document.createElement("canvas");
 let inMemCtx = inMemCanvas.getContext("2d");
 
-// Function that saves canvas state (current drawing) on a memory canvas => resizes => draws memory canvas back on original canvas.
-function resizeCanvas() {
-    inMemCanvas.width = canvas.width;
-    inMemCanvas.height = canvas.height;
-    inMemCtx.drawImage(canvas, 0, 0);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(inMemCanvas, 0, 0);
-}
+// Update mouse object {x,y} position everytime it moves relative to canvas size/positioning. (Including resize)
+let mouse = { x: 0, y: 0 };
+window.addEventListener("mousemove", function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    mouse.x = mousePos.x;
+    mouse.y = mousePos.y;
+});
 
-// Debounce function that will fire a callback method after 1 second from the last resize event.
-function setResizeHandler(callback, timeout) {
-    let timer_id = undefined;
-    window.addEventListener("resize", function () {
-        if (!timer_id) {
-            clearTimeout(timer_id);
-            timer_id = undefined;
-        }
-        timer_id = setTimeout(function () {
-            timer_id = undefined;
-            callback();
-        }, timeout);
-    });
+function getMousePos(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
 }
-setResizeHandler(resizeCanvas, 1000);
+// Debounce function that will fire a callback method after 1 second from the last resize event.
+let timer_id = undefined;
+window.addEventListener("resize", function () {
+    if (!timer_id) {
+        clearTimeout(timer_id);
+        timer_id = undefined;
+    }
+    timer_id = setTimeout(function () {
+        timer_id = undefined;
+        resizeCanvas(inMemCanvas, inMemCtx, canvas, ctx);
+    }, 1000);
+});
 
 let painting = false;
 canvas.width = window.innerWidth;
@@ -65,28 +69,12 @@ function draw(e) {
     if (!painting) return;
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
-    ctx.lineTo(e.clientX, e.clientY);
+    ctx.lineTo(mouse.x, mouse.y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(mouse.x, mouse.y);
 }
 
 canvas.addEventListener("mousedown", startPosition);
 canvas.addEventListener("mouseup", finishedPosition);
 canvas.addEventListener("mousemove", draw);
-
-// Update mouse object {x,y} position everytime it moves relative to canvas size/positioning. (Including resize)
-// let mouse = { x: 0, y: 0 };
-// window.addEventListener("mousemove", function (evt) {
-//     var mousePos = getMousePos(canvas, evt);
-//     mouse.x = mousePos.x;
-//     mouse.y = mousePos.y;
-// });
-
-// function getMousePos(canvas, event) {
-//     let rect = canvas.getBoundingClientRect();
-//     return {
-//         x: event.clientX - rect.left,
-//         y: event.clientY - rect.top
-//     };
-// }
